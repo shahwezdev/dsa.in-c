a63b66e9fb9383634b9b781fda9713ba75aee527
shahwezdev <shahwezsiddiqui5537@gmail.com>
2026-09-23 20:26:07 +0530

    Create arrays/linear-search.c
    Implement linear search

---
+++ arrays/linear-search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100];
+    int n, target;
+    int found = -1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &target);
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            found = i;
+            break;
+        }
+    }
+
+    if (found != -1)
+        printf("Element found at index %d\n", found);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
